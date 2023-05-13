3e3513e64f1383d8b48987db5306bdabe4aef0d3
Tobias Leinß <tobias.leinss@mway.io>
2023-05-13 20:09:14 +0100

    Add linea

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
-import { RainbowKitProvider, darkTheme, getDefaultWallets } from "@rainbow-me/rainbowkit"
+import {
+  RainbowKitProvider,
+  darkTheme,
+  getDefaultWallets,
+} from "@rainbow-me/rainbowkit"
 import "@rainbow-me/rainbowkit/styles.css"
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import { WagmiConfig, configureChains, createClient } from "wagmi"
@@ -8,51 +12,73 @@ import { publicProvider } from "wagmi/providers/public"
 import Dashboard from "./pages/dashboard"
 import Landingpage from "./pages/landingpage"
 
+const linea = {
+  id: 59140,
+  name: "Linea",
+  network: "linea",
+  nativeCurrency: {
+    name: "Ethereum",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: "https://rpc.goerli.linea.build",
+    },
+  },
+  blockExplorers: {
+    default: "https://explorer.goerli.linea.build",
+  },
+}
+
 function App() {
-	const { chains, provider } = configureChains(
-		[polygonMumbai, optimismGoerli],
-		[alchemyProvider({ apiKey: import.meta.env.ALCHEMY_API_KEY }), publicProvider()]
-	)
+  const { chains, provider } = configureChains(
+    [polygonMumbai, optimismGoerli, linea],
+    [
+      alchemyProvider({ apiKey: import.meta.env.ALCHEMY_API_KEY }),
+      publicProvider(),
+    ]
+  )
 
-	const { connectors } = getDefaultWallets({
-		appName: "turtleshell",
-		projectId: import.meta.env.WALLET_CONNECT_PROJECT_ID,
-		chains,
-	})
+  const { connectors } = getDefaultWallets({
+    appName: "turtleshell",
+    projectId: import.meta.env.WALLET_CONNECT_PROJECT_ID,
+    chains,
+  })
 
-	const wagmiClient = createClient({
-		autoConnect: true,
-		connectors,
-		provider,
-	})
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  })
 
-	const router = createBrowserRouter([
-		{
-			path: "/",
-			element: <Landingpage />,
-		},
-		{
-			path: "/dashboard",
-			element: <Dashboard />,
-		},
-	])
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landingpage />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
+    },
+  ])
 
-	return (
-		<WagmiConfig client={wagmiClient}>
-			<RainbowKitProvider
-				chains={chains}
-				theme={darkTheme({
-					accentColor: "#D9D9D9",
-					accentColorForeground: "#000",
-					borderRadius: "medium",
-					fontStack: "system",
-					overlayBlur: "small",
-				})}
-			>
-				<RouterProvider router={router} />
-			</RainbowKitProvider>
-		</WagmiConfig>
-	)
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={darkTheme({
+          accentColor: "#D9D9D9",
+          accentColorForeground: "#000",
+          borderRadius: "medium",
+          fontStack: "system",
+          overlayBlur: "small",
+        })}
+      >
+        <RouterProvider router={router} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
 
 export default App
